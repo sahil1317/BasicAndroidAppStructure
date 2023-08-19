52de2cfa1ac24c04c3d02ff066c16f3a1c9dd221
Sahil Sharma <sahilbhardwaj549@gmail.com>
2023-08-19 20:15:38 +0530

    enable data binding

--- app/src/main/java/com/sahil/baseapp/MainActivity.kt
+++ app/src/main/java/com/sahil/baseapp/MainActivity.kt
@@ -2,13 +2,14 @@ package com.sahil.baseapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.sahil.baseapp.databinding.ActivityMainBinding
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        val binding : ActivityMainBinding= DataBindingUtil.setContentView(this,R.layout.activity_main)
 
     }
 }
